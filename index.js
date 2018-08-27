90f03c52b2362023d0135c7ac79dfa3abc958dec
Karl Pokus <karl.pokus@gmail.com>
2018-08-27 16:51:43 +0200

    add valid ssn end point and some refactoring

--- index.js
+++ index.js
@@ -12,23 +12,93 @@ function ssns(n) {
 
 	for (i; i < n; i++) {
 		pnr = new Pernr();
-		out.push(pnr.toString());
+		out.push(pnr.toString({fullYear: true}));
 	}
 	return out.join('\n');
 }
 
-function api(req, res) {
-	const { n } = url.parse(req.url, true).query;
-	let reply;
+const handlers = {
+	GET: {
+		valid: function({ ssn }, req, cb) {
+			if (!ssn) {
+				return cb(null, new Error('Error: queryparam <ssn> missing'));
+			}
 
-	if (n && isInt(n) && +n < 1000) {
-		reply = ssns(+n);
-	} else {
-		reply = 'usage: add number of ssns to create in query param n';
+			try {
+				const pnr = new Pernr(ssn);
+				cb(pnr.isValid().toString());
+			} catch(err) {
+				cb('false');
+			}
+		},
+		gen: function({ n }, req, cb) {
+			if (!n) {
+				return cb(null, new Error('Error: queryparam <n> missing'));
+			}
+
+			if (isInt(n) && +n < 1000) {
+				cb(ssns(+n));
+			} else {
+				cb(null, new Error('Error: queryparam <n> is not a number or above the limit of 1000'));
+			}
+		}
+	},
+	POST: { // wip
+		uniq: function({ n }, req, cb) {
+			if (!n) {
+				cb(null, new Error('Error: queryparam <n> missing'));
+			}
+
+			let data = "";
+			req
+				.on('data', chunk => data += chunk)
+				.on('end', () => cb(data || "no data"))
+		}
+	}
+};
+
+function parseUrlParts(req) {
+	const parts = url.parse(req.url, true); // 2nd arg parses queryParams into {}
+	const path = parts.pathname.substr(1); // remove /
+	return {
+		method: req.method,
+		path: path ? path : "non-existent-key", // avoid looking up empty string as key on handlers
+		query: parts.query
+	}
+}
+
+function reply(res, data, err) {
+	if (err) {
+		data = usage(err.message); // err + usage
 	}
 
 	res.writeHead(200);
-	res.end(reply);
+	res.end(data);
 }
 
-srv.on('request', api).listen(PORT, () => console.log('api running..'));
+function usage(err) {
+	return `
+		${ err || "" }
+
+		api
+
+			GET /
+				returns usage
+			GET /gen?n=<number of ssns to create>
+				return n valid ssns
+			GET /valid?ssn=<ssn>
+				returns true if ssn is valid
+	`;
+}
+
+function router(req, res) {
+	const { method, path, query } = parseUrlParts(req);
+
+	if (handlers[method] && handlers[method][path]) {
+		handlers[method][path](query, req, reply.bind(null, res));
+	} else {
+		reply(res, usage()); // usage
+	}
+}
+
+srv.on('request', router).listen(PORT, () => console.log('api started on port', PORT));
